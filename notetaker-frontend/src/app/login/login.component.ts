315bdda672a3bc1b5ca9751a55bdb2de5435f0b4
DoomFungus <unspeakableferretofdoom@gmail.com>
2020-05-29 16:21:40 +0300

    QOL: Added login loader animation

--- notetaker-frontend/src/app/login/login.component.ts
+++ notetaker-frontend/src/app/login/login.component.ts
@@ -9,6 +9,7 @@ import {AuthService} from "../shared/auth.service";
 })
 export class LoginComponent implements OnInit {
 
+  loginLoadAnimation: HTMLElement;
   constructor(private loginService:AuthService, @Inject('M') private M: any) { }
 
   @Output()
@@ -20,6 +21,7 @@ export class LoginComponent implements OnInit {
       const elems = document.querySelectorAll('.log-modal');
       const instances = self.M.Modal.init(elems, {dismissible: false});
       self.M.Modal.getInstance(document.getElementById("login-modal")).open()
+      self.loginLoadAnimation = document.getElementById("login_loader")
     });
   }
 
@@ -31,6 +33,7 @@ export class LoginComponent implements OnInit {
 
   onLogin(){
     const self = this;
+    this.loginLoadAnimation.style.display = 'block'
     this.loginService.signIn(this.model.username, this.model.password).subscribe(
       data => self.onSuccessfulLogin(self.model.username, data.accessToken, data.refreshToken),
       error => self.onUnsuccessfulLogin(error)
@@ -39,6 +42,7 @@ export class LoginComponent implements OnInit {
 
   onSignup(){
     const self = this;
+    this.loginLoadAnimation.style.display = 'block'
     this.loginService.signUp(this.model.username, this.model.password).subscribe(
       data => self.onSuccessfulLogin(self.model.username, data.accessToken, data.refreshToken),
       error => self.onUnsuccessfulSignup(error)
@@ -47,21 +51,20 @@ export class LoginComponent implements OnInit {
 
   onSuccessfulLogin(username:string, accessToken:string, refreshToken:string){
     this.onLoggedInEvent.emit(this.model.username)
-    const accessExpiresAt = JSON.parse(atob(accessToken.split('.')[1])).exp * 1000
-    const refreshExpiresAt = JSON.parse(atob(refreshToken.split('.')[1])).exp * 1000
+    this.loginLoadAnimation.style.display = 'none'
     localStorage.setItem("access_token", accessToken)
-    localStorage.setItem("access_expires_at", accessExpiresAt.toString())
     localStorage.setItem("refresh_token", refreshToken)
-    localStorage.setItem("refresh_expires_at", refreshExpiresAt.toString())
     this.M.Modal.getInstance(document.getElementById("login-modal")).close();
     this.model.headingMessage = "Please enter your credentials"
   }
 
   onUnsuccessfulLogin(error){
+    this.loginLoadAnimation.style.display = 'none'
     this.model.headingMessage = "Please verify your credentials"
   }
 
   onUnsuccessfulSignup(error){
+    this.loginLoadAnimation.style.display = 'none'
     this.model.headingMessage = "Sorry, this username is taken"
   }
 }
